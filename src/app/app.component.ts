f22e01692be5d44a0275b791470fb24e0e398318
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-20 12:55:51 +0530

    user4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { DUMMY_USERS } from '../user/dummy-users';
 import { UserComponent } from '../user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent],
+  imports: [HeaderComponent,UserComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  SelectedUserId = 'u1';
+  
+  get selectedUser()
+  {
+    return this.users.find((user) => user.id === this.SelectedUserId);
+
+  }
+  onSelectedUser(id : string)
+  {
+    this.SelectedUserId = id;
+  }
 }
