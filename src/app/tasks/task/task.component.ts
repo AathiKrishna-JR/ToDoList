0405370d842b9e1235a47cbaaa4e6d5757fa32d7
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-21 15:06:30 +0530

    s2

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -6,7 +6,7 @@ import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  imports: [CardComponent,DatePipe,TasksService],
+  imports: [CardComponent,DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
